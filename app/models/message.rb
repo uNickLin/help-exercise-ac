a3bb2569ec90a93879f9d353cbd510cdee79be92
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-04-28 16:15:52 +0800

    ActiveRecord Scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,7 @@ class Message < ApplicationRecord
     self.comments.last.try(:content).try(:truncate, 20)
   end
 
+  scope :pending, ->{ where( status: "pending" ) }
+  scope :completed, ->{ where( status: "completed" ) }
+
 end
